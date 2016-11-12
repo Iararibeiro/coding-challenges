174dcae2a6a051b84da0d3a9225787ddbcd7b29a
IaraRibeiro <iara.prbr@gmail.com>
2016-11-12 20:02:14 -0300

    stack in python

--- data estructures/stack.py
+++ data estructures/stack.py
@@ -1,5 +1,5 @@
 """ 
-This algorithm implements a generic version of a Stack in javascript, the idea
+This algorithm implements a generic version of a Stack in python, the idea
 with the algorithm in this directory is have a source for future reference in
 problems from codility or similar sites/problems.
 
@@ -8,53 +8,52 @@ a book by Thomas H. Cormen.
 
 @author: Iara Ribeiro - 11/12/2016.
 """ 
+class Stack():
+	
+	def __init__(self):
+		self.top = 0
+		self.S = []
 
-top = 0;
-S = [];
+	def stackEmpty(self):
+		if(self.top == 0):
+			return "true"
+		return "false"
+	
+	def push(self,x):
+		self.top= self.top + 1
+		self.S.insert(self.top, x)
 
-def stackEmpty(S):
-	if(top == 0):
-		return "true"
-	return "false"
-
-def push(S,x):
-	top += 1
-	S[top] = x
-
-
-"""
-function pop(S){
-	if(stackEmpty(S)){
-		return "stack empty";
-	}else{
-		top -= 1;
-		return S[top + 1];
-	}
-}
-"""
-
-def print_stack(S):
-	result = "[";
-	for i in range(1,top):
-		if( i ==1):
-			result += S[i];
+	def pop(self):
+		if(self.top == 0):
+			return "underflow"
 		else:
-			result = result + "," + S[i];
-	return result+"]";
+			self.S.remove(self.S[(self.top-1)])
+			self.top = self.top - 1
+	
+	def print_stack(self):
+		result = "[";
+		for i in range(0,self.top):
+			if(i==0):
+				result += self.S[i]
+			else:
+				result = result + "," + self.S[i]
+		return result+"]"
 
+	def test(functionToTest, args, expected):
+		result = functionToTest(args)
+		print "Expected " + expected + ", was " + result		
 
-def test(functionToTest, args, expected):
-	result = functionToTest(args)
-	print "Expected " + expected + ", was " + result
-
-test(stackEmpty, S, "true");
-push(S,"banana");
-test(print_stack, [S], "[banana]");
-#push(S,"apple");
-#test(print, [S], "[banana,apple]");
-#pop(S);
-#test(print, [S], "[banana]");
-#push(S,"pineapple");
-#test(print, [S], "[banana,pineapple]");
-#pop(S);
-#test(print, [S], "[]");
+if __name__ == '__main__':
+    s = Stack()
+    print("Expected " + s.stackEmpty() + ", was " + "true")
+    s.push("banana")
+    print("Expected " +"[banana]" + ", was " + s.print_stack())
+    s.push("apple")
+    print("Expected " +"[banana,apple]"+ ", was " +s.print_stack())
+    s.pop()
+    print("Expected " +"[banana]" + ", was " + s.print_stack())    
+    s.push("pineapple")
+    print("Expected " +"[banana,pineapple]"+ ", was " +s.print_stack())
+    s.pop()
+    s.pop()
+    print("Expected " +"[]"+ ", was " +s.print_stack())
