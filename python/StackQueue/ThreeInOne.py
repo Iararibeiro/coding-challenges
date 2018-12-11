ff7fd3b83b39c6147ddbb06158f62d7e02affc2e
Iara Ribeiro <iara.prbr@gmail.com>
2018-12-10 21:35:53 -0500

    first version of three stacks in one

---
+++ python/StackQueue/ThreeInOne.py
@@ -0,0 +1,18 @@
+# Problem: use one array to implement three stacks
+class ThreeInOneStack:
+
+    def __init__(self, number_of_stacks, stack_capacity, values):
+		self.stack_one = []
+        self.stack_two = []
+        self.stack_three = []
+        self.stack_capacity = stack_capacity
+
+    def push(self, value):
+        #include the value in the stack with more space
+        #if the stack number isnt given, if the stack choose is full
+        #the value is added to the one with more space
+        return None
+
+    def pop(self):
+        #remove the value from the bigger stack
+        return None
