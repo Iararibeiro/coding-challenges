d6434ad1c412a74c6375c779539faaa43cbdc056
Iara Ribeiro <iara.prbr@gmail.com>
2019-02-13 17:45:17 -0500

    including first version of graphs implementation

---
+++ python/trees/graphs.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+class Graph:
+
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def addEdge(self,u,v):
+        self.graph[u].append(v)
+
+    def bfs(graph, start):
+        visited, queue = set(), [start]
+        while queue:
+            vertex = queue.pop(0)
+            if vertex not in visited:
+                visited.add(vertex)
+                queue.extend(graph[vertex] - visited)
+        return visited
+
+    def dfs(graph, start, visited=None):
+        if visited is None:
+            visited = set()
+        visited.add(start)
+        for next in graph[start] - visited:
+            dfs(graph, next, visited)
+        return visited
