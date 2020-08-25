25be15a59a538686bcd268f7dec39a8c2b62f81c
Iara Ribeiro <iara.prbr@gmail.com>
2020-08-25 10:32:53 -0400

    update reverse integer to meet solution

--- python/code-problems/ReverseInteger.py
+++ python/code-problems/ReverseInteger.py
@@ -3,6 +3,9 @@ import sys
 def reverseInteger(number):
     rev = 0
     negativeNumber = False
+    negLimit= -0x80000000
+    posLimit= 0x7fffffff
+
     if (number < 0):
         negativeNumber = True
         number = number * -1
@@ -17,9 +20,14 @@ def reverseInteger(number):
         rev = rev * 10 + pop
 
     if negativeNumber:
-        return rev * -1
-
+        rev = rev * -1
+        if rev < negLimit:
+            return 0
     return rev
+        else:
+        if rev > posLimit:
+            return 0
+        return rev
 
 print reverseInteger(123)
 print reverseInteger(-123)
