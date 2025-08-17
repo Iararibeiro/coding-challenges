261c07f0feca8c3186d8aa9ff44d5cce5b8f1d86
Iara Ribeiro <Iara.prbr@gmail.com>
2025-08-17 09:25:28 -0400

    Optimize Container With Most Water solution
    Refactored the brute-force approach to a two-pointer technique for improved efficiency. Added type hints, docstring, and replaced print statements with assertions for test cases.

--- medium/arrays/ContainerWithMostWater.py
+++ medium/arrays/ContainerWithMostWater.py
@@ -1,24 +1,38 @@
-def maxArea(height):
+"""
+You are given an integer array height of length n. There are n vertical lines drawn such that the 
+two endpoints of the ith line are (i, 0) and (i, height[i]).
+Find two lines that together with the x-axis form a container, such that the container 
+contains the most water.
+Return the maximum amount of water a container can store.
+Notice that you may not slant the container.
+"""
+
+from typing import List
+
+
+def maxArea(height: List[int]) -> int:
     result = 0
+    n = len(height) - 1
+    p1 = 0
+    p2 = n
 
-    for i in range(0, len(height)):
-    
-        for j in range(i+1, len(height)):
-            if height[i] <= height[j]:
-                containerHeight = height[i]
-            else:
-                containerHeight = height[j]
-            lenght = j - i
-            
-            area = containerHeight * lenght
-            if area > result:
-                result = area
+    while n > 0:
+        containerHeight = min(height[p1], height[p2])
+        area = containerHeight * n
 
-    print(result)
+        if area > result:
+            result = area
+        
+        if height[p1] < height[p2]:
+            p1+=1
+        else:
+            p2-=1
+        
+        n -= 1
 
+    return result
 
-
-maxArea([1,8,6,2,5,4,8,3,7])
-maxArea([1,1])
-maxArea([4,3,2,1,4])
-maxArea([1,2,1])
+assert maxArea([1,8,6,2,5,4,8,3,7]) == 49
+assert maxArea([1,1]) == 1
+assert maxArea([4,3,2,1,4]) == 16
+assert maxArea([1,2,1]) == 2
